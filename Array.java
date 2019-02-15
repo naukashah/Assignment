1bdc60af1e5d0d0029144f6cb47d0091acda2e31
naukashah <47592363+naukashah@users.noreply.github.com>
2019-02-14 23:26:32 -0800

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+package Assignment;
+//Define a array with 10 integers and print all the elements with its index number.
+public class Array {
+	//int a[] = new int[10];
+	
+	public static void main(String[] args) {
+		int[] num = {12, 4, 5, 2, 5};
+
+	      for (int i = 0; i < 5; ++i) {
+	         System.out.println("Element at index " + i +": " + num[i]);
+	      }
+		
+	}
+
+}
